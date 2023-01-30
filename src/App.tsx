8aa6b611cc27f2a5dc281c098a62be6ab274c1e3
Mark Jerly Bundalian <mbundalian@fullscale.io>
2023-01-30 20:40:59 +0800

    Add delete character function

--- src/App.tsx
+++ src/App.tsx
@@ -65,6 +65,23 @@ function App() {
     setExpression(result.toString())
   }
 
+  const deleteCharacters = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    e. preventDefault()
+
+    if (expression.length > 1) {
+      if (expression[expression.length - 1] === ".") {
+        const newExpression = expression.slice(0, -1)
+        setExpression(newExpression)
+        setIsDecimalPressed(false)
+      } else {
+        const newExpression = expression.slice(0, -1)
+        setExpression(newExpression)
+      }
+    } else {
+      setExpression("0")
+    }
+  }
+
   return (
     <div className="h-screen w-100 flex flex-col justify-center items-center bg-backdrop">
       <div id="calc" className="bg-front w-2/3 xs:w-1/3 md:w-2/3 lg:w-1/2 xl:w-1/4">
@@ -96,7 +113,7 @@ function App() {
           <button className="py-4 text-center text-3xl font-bold bg-operator-keys text-operator-color w-1/4 hover:bg-hover">+</button>
 
           {/* Fifth Row */}
-          <button className="py-4 text-center font-semibold text-keys-color text-3xl bg-keys w-1/4 hover:bg-hover">&lt;</button>
+          <button onClick={(e) => deleteCharacters(e)} className="py-4 text-center font-semibold text-keys-color text-3xl bg-keys w-1/4 hover:bg-hover">&lt;</button>
           <button onClick={(e) => showDigitInScreen(e)} className="py-4 text-center font-thin text-main-color text-3xl bg-keys w-1/4 hover:bg-hover">0</button>
           <button onClick={(e) => showDecimalPointInScreen(e)} className="py-4 text-center font-thin text-main-color text-3xl bg-keys w-1/4 hover:bg-hover">.</button>
           <button className="py-4 text-center text-3xl font-bold bg-equals text-white w-1/4">=</button>
